f54801c49720c4c200c5a1fe35fb427d7c2ed25b
Eric Quy Nguyen <nguyen.ngocquy@team.telstra.com>
2018-05-16 17:13:12 +0800

    Add less loader

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './ui/App';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './ui/Layout.css';
+import './ui/Layout.less';
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render( < App / > , document.getElementById('app-content'));
